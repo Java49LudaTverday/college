41e9261da8451635e556f4562e6fae243548f1a1
LudaTverday <Ludachka22@gmail.com>
2023-12-04 23:27:53 +0200

    initial code

---
+++ src/main/java/telran/college/entities/Student.java
@@ -0,0 +1,12 @@
+package telran.college.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+public class Student extends Person {
+
+}
---
+++ src/main/java/telran/college/entities/Subject.java
@@ -0,0 +1,34 @@
+package telran.college.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import telran.college.dto.SubjectType;
+
+@Entity
+@Table(name = "subjects")
+@Getter
+@NoArgsConstructor
+public class Subject {
+	@Id
+	long id;
+	@Column(nullable=false)
+	String name;
+	int hours;
+	@ManyToOne
+	@JoinColumn(name="lecturer_id")
+	Lecturer lecturer;
+	@Enumerated(value = EnumType.STRING)
+	@Column(nullable = false)
+	SubjectType type;
+
+}
