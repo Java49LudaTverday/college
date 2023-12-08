735cdc9cc33688033fa68ad2742d75492e9971a8
LudaTverday <Ludachka22@gmail.com>
2023-12-08 22:48:27 +0200

    implementation test for all methods using Map

--- src/test/java/telran/college/CollegeServiceTest.java
+++ src/test/java/telran/college/CollegeServiceTest.java
@@ -2,21 +2,16 @@ package telran.college;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.minidev.json.JSONObject;
-import telran.college.dto.LecturerHours;
+import telran.college.dto.LecturerMark;
 import telran.college.dto.StudentMark;
 import telran.college.service.CollegeService;
 
@@ -24,12 +19,10 @@ import telran.college.service.CollegeService;
 @Sql(scripts = { "db_test.sql" })
 class CollegeServiceTest {
 	@Autowired
-	CollegeService collegeService;
-	@Autowired
-	ObjectMapper objectMapper;
+	CollegeService collegeService;	
 
 	@Test
-	@Disabled
+//	@Disabled
 	void bestStudentsTypeTest() {
 		List<String> students = collegeService.bestStudentsSubjectType("BACK_END", 2);
 		String[] expected = { "David", "Yosef" };
@@ -37,24 +30,35 @@ class CollegeServiceTest {
 	}
 
 	@Test
-	@Disabled
+//	@Disabled
 	void studentsAvgScoreTest() {
 		List<StudentMark> studentsMark = collegeService.studentsAvgMarks();
-		StudentMark[] expected = {};
-		studentsMark.forEach(sm -> System.out.printf("student: %s, avg score: %d\n", sm.getName(), sm.getScore()));
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("David", 96);
+		expected.put("Vasya", 83);
+		expected.put("Yosef", 78);
+		expected.put("Sara", 80);
+		expected.put("Rivka", 95);
+		studentsMark.forEach(sm -> System.out.printf("student: %s, avg score: %d\n", 
+				sm.getName(), sm.getScore()));
+		runTest(studentsMark, expected);
 	}
 
 	@Test
-	@Disabled
+//	@Disabled
 	void mostLecturersHoursTest() throws Exception {
-		List<LecturerHours> lecturersHours = collegeService.mostLecturerHours(3);
-		List<LecturerHours> expected = new ArrayList<>();
+		List<LecturerMark> lecturersHours = collegeService.mostLecturerHours(3);
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("Abraham", 225);
+		expected.put("Mozes", 130);
+		expected.put("Sockratus", 65);		
 		lecturersHours.forEach(lh -> System.out.printf("lecture: %s, hours: %d\n", lh.getName(), lh.getHours()));
-		assertArrayEquals(expected.toArray(), lecturersHours.toArray());
+		assertEquals(expected.size(), lecturersHours.size());
+		lecturersHours.forEach(lh -> assertEquals(lh.getHours(), expected.get(lh.getName())));
 	}
 	
 	@Test
-	@Disabled
+//	@Disabled
 	void studentsHavingLessScoreTest() throws Exception {
 		List<String> lecturersHours = collegeService.studentsHavingLessScore(80);
 		String[] expected = {"Sara,Beersheva","Vasya,Rehovot","Yosef,Rehovot"};		
@@ -65,17 +69,39 @@ class CollegeServiceTest {
 	@Test
 //	@Disabled
 	void lecturersFromCityTest() {
-		List<String> lecturers = collegeService.allLecturersFromCity("Rehovot");
+		List<String> lecturers = collegeService.allLecturersByCity("Rehovot");
 		String[] expected = {"Sockratus,057-7664821"};
 		lecturers.forEach(l -> System.out.println(l));
 		assertArrayEquals(expected, lecturers.toArray());
 	}
+	@Test
+//	@Disabled
+	void studentsBornInMonthTest() {
+		int october = 10;
+		List<String> students = collegeService.studentsBornInMonth(october);
+		String[] expected = {"Vasya,054-1234567", "Yakob,051-6677889"};
+		students.forEach(st -> System.out.println(st));
+		assertArrayEquals(expected, students.toArray());
+	}
+	@Test
+//	@Disabled
+	void subjectsScoresByStudentTest() throws Exception {
+		String student = "Sara";
+		List<StudentMark> subjectMark = collegeService.subjectsAndScoresOfStudent(student);
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("Java Core", 70);
+		expected.put("Java Technologies", 65);
+		expected.put("HTML/CSS", 90);
+		expected.put("JavaScript",95);		
+//		("Java Core",70, "Java Technologies",65, "HTML/CSS",90, "JavaScript",95);
+		subjectMark.forEach(st -> System.out.printf("subject: %s, score: %d\n",st.getName(), st.getScore()));
+		runTest(subjectMark, expected);		
+		
+	}
 
-	private JSONObject getJSONObject(String name, String hours) {
-		JSONObject lecturer = new JSONObject();
-		lecturer.put("name", name);
-		lecturer.put("hours", hours);
-		return lecturer;
+	private void runTest(List<StudentMark> subjectMark, Map<String, Integer> expected) {
+		assertEquals(expected.size(), subjectMark.size());
+		subjectMark.forEach(sm -> assertEquals(sm.getScore(), expected.get(sm.getName())));
 	}
 
 }
