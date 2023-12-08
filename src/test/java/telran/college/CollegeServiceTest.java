885a5493509ec698e851bf60d2213803fc8fdfcd
LudaTverday <Ludachka22@gmail.com>
2023-12-08 09:51:53 +0200

    CollegeServiceTest: query avg score, best students

---
+++ src/test/java/telran/college/CollegeServiceTest.java
@@ -0,0 +1,33 @@
+package telran.college;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import telran.college.dto.StudentMark;
+import telran.college.service.CollegeService;
+
+@SpringBootTest
+@Sql(scripts= {"db_test.sql"})
+class CollegeServiceTest {
+@Autowired
+CollegeService collegeService;
+
+	@Test
+	void bestStudentsTypeTest() {
+		List<String> students = collegeService.bestStudentsSubjectType("BACK_END", 2);
+		String[] expected = {"David", "Yosef"};
+		assertArrayEquals(expected, students.toArray());
+	}
+	@Test
+	void studentsAvgScoreTest() {
+		List<StudentMark> studentsMark = collegeService.studentsAvgMarks();
+		studentsMark.forEach(sm -> System.out.printf("student: %s, avg score: %d\n", sm.getName(),sm.getScore()));
+	}
+
+}
