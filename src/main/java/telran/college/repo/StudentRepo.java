6920889ddd61cd92c4fad738be8e377143193a37
LudaTverday <Ludachka22@gmail.com>
2023-12-08 15:02:06 +0200

    realised queries bestStudentsBySubject, studentsAvgMarks,havingLessscore

--- src/main/java/telran/college/repo/StudentRepo.java
+++ src/main/java/telran/college/repo/StudentRepo.java
@@ -10,13 +10,17 @@ import telran.college.entities.*;
 
 public interface StudentRepo extends JpaRepository<Student, Long> {
 	String JOIN_STUDENTS_MARKS = "FROM students_lecturers st join marks m on stid=st.id ";
-	String JOIN_ALL = JOIN_STUDENTS_MARKS
-			+ "join subjects sb on sb.id=suid ";
-	@Query(value="SELECT st.name as name " + JOIN_ALL + "where type=:type "
+	String JOIN_ALL = JOIN_STUDENTS_MARKS + "join subjects sb on sb.id=suid ";
+
+	@Query(value = "SELECT st.name as name " + JOIN_ALL + "where type=:type "
 			+ "group by st.name order by avg(score) desc limit :nStudents", nativeQuery = true)
-List<String> findBestStudentsSubjectType(String type, int nStudents);
-	
-	 @Query(value="SELECT st.name as name, round(avg(score)) as score " + 
-JOIN_STUDENTS_MARKS + "group by st.name order by avg(score) desc", nativeQuery=true)
+	List<String> findBestStudentsSubjectType(String type, int nStudents);
+
+	@Query(value = "SELECT st.name as name, round(avg(score)) as score " + JOIN_STUDENTS_MARKS
+			+ "group by st.name order by avg(score) desc", nativeQuery = true)
 	List<StudentMark> studentsAvgMarks();
+
+	@Query(value = "SELECT name, city " + JOIN_STUDENTS_MARKS
+			+ "GROUP BY name having min(m.score)< :score", nativeQuery = true)
+	List<String> findStudentsHavingLessScore(int score);
 }
