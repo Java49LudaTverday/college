4b5fc3f4b7afcd6e91b652162163cbe527ba9be0
LudaTverday <Ludachka22@gmail.com>
2023-12-16 10:10:12 +0200

    realisation add and update methods

--- src/main/java/telran/college/service/CollegeServiceImpl.java
+++ src/main/java/telran/college/service/CollegeServiceImpl.java
@@ -3,20 +3,18 @@ package telran.college.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.lang.Override;
 import lombok.RequiredArgsConstructor;
-import telran.college.dto.LecturerHours;
-import telran.college.dto.NamePhone;
-import telran.college.dto.NameScore;
-import telran.college.dto.StudentCity;
-import telran.college.dto.SubjectNameScore;
-import telran.college.dto.SubjectType;
-import telran.college.entities.Person;
-import telran.college.entities.Student;
+import telran.college.dto.*;
+import telran.college.entities.*;
 import telran.college.repo.*;
+import telran.exceptions.NotFoundException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly=true)
 public class CollegeServiceImpl implements CollegeService {
 	final StudentRepo studentRepo;
 	final LecturerRepo lecturerRepo;
@@ -52,7 +50,6 @@ public class CollegeServiceImpl implements CollegeService {
 	}
 	@Override
 	public List<NamePhone> lecturersCity(String city) {
-		//TODO method with named query
 		return lecturerRepo.findByCity(city);
 	}
 	@Override
@@ -60,6 +57,104 @@ public class CollegeServiceImpl implements CollegeService {
 		
 		return markRepo.findByStudentName(studentName);
 	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public PersonDto addStudent(PersonDto personDto) {
+		if(studentRepo.existsById(personDto.id())) {
+			throw new IllegalStateException(String.format("Student with id %d already exists", personDto.id()));
+		}
+		studentRepo.save(new Student(personDto));
+		return personDto;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public PersonDto addLecturer(PersonDto personDto) {
+		if(lecturerRepo.existsById(personDto.id())) {
+			throw new IllegalStateException(String.format("Lecturer with id %d already exists", personDto.id()));
+		}
+		lecturerRepo.save(new Lecturer(personDto));
+		return personDto;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public SubjectDto addSubject(SubjectDto subjectDto) {
+		Lecturer lecturer = null;
+		if(subjectDto.lecturerId() != null) {
+			lecturer = lecturerRepo.findById(subjectDto.lecturerId()).orElseThrow(
+				() -> new NotFoundException(getNotExistsMessage("lecturer", subjectDto.lecturerId())));
+		}
+		Subject subject = new Subject(subjectDto);
+		subject.setLecturer(lecturer);
+		subjectRepo.save(subject);
+		return subjectDto;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public MarkDto addMark(MarkDto markDto) {
+		Student student = studentRepo.findById(markDto.studentId()).orElseThrow(() -> 
+		new NotFoundException(getNotExistsMessage("student", markDto.studentId())));
+		Subject subject = subjectRepo.findById(markDto.subjectId()).orElseThrow(()-> 
+		new NotFoundException(getNotExistsMessage("subject", markDto.subjectId())));
+		Mark mark = new Mark();
+		mark.setFields(markDto.score(), student, subject);
+		markRepo.save(mark);			
+		return markDto;	
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public PersonDto updateStudent(PersonDto personDto) {
+		Student student = studentRepo.findById(personDto.id()).orElseThrow(
+				() -> new NotFoundException(getNotExistsMessage("student", personDto.id())));
+		student.setCity(personDto.city());
+		student.setPhone(personDto.phone());
+		return personDto;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public PersonDto updateLecturer(PersonDto personDto) {
+		Lecturer lecturer = lecturerRepo.findById(personDto.id()).orElseThrow(() -> 
+		new NotFoundException(getNotExistsMessage("lecturer", personDto.id())));
+		lecturer.setCity(personDto.city());
+		lecturer.setPhone(personDto.phone());
+		return personDto;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public PersonDto deleteLecturer(long id) {
+		//find Lecturer by id (with possible exception NotFoundException)
+		//find all subjects with a given lecturer id 
+		// update all subjects with being deleted lecturer by setting null in field Lecturer
+		//lecturerRepo.delete(lecturer);
+		//returns lecturer.build();
+		return null;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public SubjectDto deleteSubject(long id) {
+		//find subject by id (with possible exception NotFoundException)
+		//delete all marks with a given subject
+		//subjectRepo.delete(lecturer);
+		//returns subject.build();
+		return null;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public List<PersonDto> deleteStudentsHavingScoresLess(int nScores) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
+	private String getNotExistsMessage(String object, long id) {
+		return String.format("%s with id %d not exists", object, id );
+	}
 
 }
