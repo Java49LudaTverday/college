f2b308d3d8f3fb5e3b380c5d37d4bc358106e90f
LudaTverday <Ludachka22@gmail.com>
2023-12-16 16:07:56 +0200

    added method findByLecturer

--- src/main/java/telran/college/repo/SubjectRepo.java
+++ src/main/java/telran/college/repo/SubjectRepo.java
@@ -14,4 +14,5 @@ public interface SubjectRepo extends JpaRepository<Subject, Long> {
 	@Query(value="select lecturer.name as name, sum(hours) as hours from "
 			+ "Subject group by lecturer.name order by sum(hours) desc limit :nLecturers")
 		List<LecturerHours> findLecturersMostHours(int nLecturers);
+	List<Subject> findByLecturer(Lecturer lecturer);
 }
